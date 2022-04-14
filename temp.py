30a15f9f81b3f77fb4e5c8fe1e7337ef7efa189f
MUHAMMADALIBOBOKHON2003 <MuhammadaliBOBOKHON20031201@gmail.com>
2022-04-15 01:21:13 +0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,9 +1,1 @@
-print("Hello world")
-users = ['alisher1983','aziza','yasina' 'umar']
 
-login = input("Yangi login tanlang:" )
-
-if login in users:
-    print('Login band, yangi login tanalng!')
-else:
-    print("Xush kelibsiz!")
